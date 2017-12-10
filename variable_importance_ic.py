aef44acf5925b32076d75f4ff6cc19057c1ff6d0
Brian Williamson <brianw26@uw.edu>
2017-12-09 16:48:26 -0800

    Updated IC to work on numpy arrays

--- variable_importance_ic.py
+++ variable_importance_ic.py
@@ -13,25 +13,25 @@
 ## ------ ---- --------
 #######################################################################################################
 ## FUNCTION: variableImportanceIC
-## ARGS:   full - model fit to the full data
-##      reduced - model fit to the reduced data
-##            y - outcome
+## ARGS:   full - model fit to the full data (numpy array)
+##      reduced - model fit to the reduced data (numpy array)
+##            y - outcome (numpy array)
 ## standardized - standardized or non-standardized parameter
-##        logit - logit scale?
 ## RETURNS: influence curve fit to the data
 def variableImportanceIC(full = None, reduced = None, y = None, standardized = True):
     import numpy as np
     import pdb
-    pdb.set_trace()
     ## calculate naive estimates
-    naive_j = np.mean((full - reduced) ** 2)
-    naive_var = np.mean((y - np.mean(y)) ** 2)
+    pdb.set_trace()
+
+    naive_j = np.mean(np.square(full - reduced))
+    naive_var = np.mean(np.square(y - np.mean(y)))
     
     ## now calculate ic
     if(standardized):
-        ret = (2*(y - full)*(full - reduced) + (full - reduced) ** 2 - naive_j)/naive_var - ((y - np.mean(y)) ** 2 - naive_var)*naive_j/(naive_var ** 2)
+        ret = (2*np.dot(np.transpose(y - full), full - reduced) + np.square(full - reduced) - naive_j)/naive_var - (np.square(y - np.mean(y)) - naive_var)*naive_j/(naive_var ** 2)
         
     else:
-        ret = 2*(y - full)*(full - reduced) + (full - reduced) ** 2 - naive_j
+        ret = (2*np.dot(np.transpose(y - full), full - reduced) + np.square(full - reduced) - naive_j)
     
     return ret
